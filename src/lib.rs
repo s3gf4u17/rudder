f0c695cefa312396bea0f520a27d11cd16368128
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-09-23 02:00:10 +0200

    basic routing

--- src/lib.rs
+++ src/lib.rs
@@ -3,36 +3,41 @@ use std::io::BufReader;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
+use std::collections::HashMap;
 
 pub struct App {
     host:String,
     port:String,
+    paths:HashMap<String,fn()->String>,
 }
 
 impl App {
     pub fn new(host:&str,port:&str) -> App {
         let host = host.to_string();
         let port = port.to_string();
-        App{host,port}
+        let paths:HashMap<String,fn()->String> = HashMap::new();
+        App{host,port,paths}
+    }
+    pub fn add_path(&mut self,path:&str,f:fn()->String) {
+        self.paths.insert(String::from(path),f);
     }
     pub fn run(&self) {
         let listener = TcpListener::bind(format!("{}:{}",self.host,self.port)).unwrap();
         for stream in listener.incoming() {
-            let stream = stream.unwrap();
-            handle_connection(stream);
+            let mut stream = stream.unwrap();
+
+            let buf_reader = BufReader::new(&mut stream);
+            let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
+            // println!("request: {:#?}",http_request);
+            let path: Vec<_> = http_request[0].split(" ").collect();
+            println!("request: {:#?}",path[1]);
+
+            let func = self.paths.get(path[1]).unwrap();
+            let contents = func();
+            let status_line = "HTTP/1.1 200 OK";
+            let length = contents.len();
+            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+            stream.write_all(response.as_bytes()).unwrap();
         }
     }
-}
-
-pub fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-    .lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
-    println!("request: {:#?}",http_request);
-
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use rudder::*;
+use std::fs;
+
+fn index() -> String {
+    fs::read_to_string("templates/index.html").unwrap()
+}
+
+fn test() -> String {
+    fs::read_to_string("templates/test.html").unwrap()
+}
 
 fn main() {
-    let app = rudder::App::new("127.0.0.1","7878");
+    let mut app = rudder::App::new("127.0.0.1","7878");
+
+    app.add_path("/",index);
+    app.add_path("/test",test);
+
     app.run();
 }
