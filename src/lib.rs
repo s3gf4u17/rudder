4808c1a1d184e476adfb5230165399bf3ffe8cd7
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-09-23 15:01:37 +0200

    code formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,73 @@
+use std::collections::HashMap;
+use std::fs;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::fs;
-use std::collections::HashMap;
 
 pub struct App {
-    host:String,
-    port:String,
-    router:HashMap<String,fn()->Response>,
+    host: String,
+    port: String,
+    router: HashMap<String, fn() -> Response>,
 }
 
 pub struct Response {
-    status:String,
-    length:usize,
-    contents:String,
+    status: String,
+    length: usize,
+    contents: String,
 }
 
 impl App {
-    pub fn new(host:&str,port:&str) -> App {
+    pub fn new(host: &str, port: &str) -> App {
         let host = host.to_string();
         let port = port.to_string();
-        let router: HashMap<String,fn()->Response> = HashMap::new();
-        App{host,port,router}
+        let router: HashMap<String, fn() -> Response> = HashMap::new();
+        App { host, port, router }
     }
-    pub fn handle_route(&mut self,path:&str,f:fn()->Response) {
-        self.router.insert(String::from(path),f);
+    pub fn handle_route(&mut self, path: &str, f: fn() -> Response) {
+        self.router.insert(String::from(path), f);
     }
     pub fn listen(&self) {
-        let listener = TcpListener::bind(format!("{}:{}",self.host,self.port)).unwrap();
+        let listener = TcpListener::bind(format!("{}:{}", self.host, self.port)).unwrap();
         for stream in listener.incoming() {
             let mut stream = stream.unwrap();
             let buf_reader = BufReader::new(&mut stream);
-            let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
-            println!("request: {:#?}",http_request);
+            let http_request: Vec<_> = buf_reader
+                .lines()
+                .map(|result| result.unwrap())
+                .take_while(|line| !line.is_empty())
+                .collect();
+            println!("request: {:#?}", http_request);
 
             let path: Vec<_> = http_request[0].split(" ").collect();
             let handler = self.router.get(path[1]).unwrap();
             let response = handler();
-            let responsebytes = format!("{}\r\nContent-Length: {}\r\n\r\n{}",response.status,response.length,response.contents);
+            let responsebytes = format!(
+                "{}\r\nContent-Length: {}\r\n\r\n{}",
+                response.status, response.length, response.contents
+            );
             stream.write_all(responsebytes.as_bytes()).unwrap();
         }
     }
 }
 
 impl Response {
-    pub fn HTMLstring(contents:String) -> Response {
+    pub fn HTMLstring(contents: String) -> Response {
         let status = String::from("HTTP/1.1 200 Ok");
         let length = contents.len();
-        Response{status,length,contents}
+        Response {
+            status,
+            length,
+            contents,
+        }
+    }
+    pub fn HTMLdynamic<T>(mut contents: String, context: T) -> Response {
+        let status = String::from("HTTP/1.1 200 Ok");
+        let length = contents.len();
+        Response {
+            status,
+            length,
+            contents,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use rudder::*;
 use std::fs;
 
-const template : &str = "<h1>test template</h1>";
+const template: &str = "<h1>test template for user: {{name}}</h1>";
+
+struct User {
+    name: String,
+}
 
 fn controller() -> rudder::Response {
     let response = rudder::Response::HTMLstring(String::from(template));
+    let user = User {
+        name: String::from("Alisson"),
+    };
+    let response = rudder::Response::HTMLdynamic::<User>(String::from(template), user);
     response
 }
 
 fn main() {
-    let mut app = rudder::App::new("127.0.0.1","7878");
-    app.handle_route("/",controller);
+    let mut app = rudder::App::new("127.0.0.1", "7878");
+    app.handle_route("/", controller);
     app.listen();
 }
